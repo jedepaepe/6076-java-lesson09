aee3c7448b06e7f65b118b84be7fd61495142cc7
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-12 13:45:40 +0100

    gitignore

---
+++ src/Ex1Program.java
@@ -0,0 +1,5 @@
+public class Ex1Program {
+    public static void main(String[] args) {
+        System.out.println("hello epfc");
+    }
+}
---
+++ src/MesVariables.java
@@ -0,0 +1,8 @@
+public class MesVariables {
+    public static void main(String[] args) {
+        int variable1 = 5;
+        String variable2 = "hello java";
+        System.out.println(variable1);
+        System.out.println(variable2);
+    }
+}
