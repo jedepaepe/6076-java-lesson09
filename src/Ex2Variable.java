359fb29e77a106325e1fe3ba3c064a304b953fc9
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-12 13:50:20 +0100

    exercice

---
+++ src/Ex2Variable.java
@@ -0,0 +1,10 @@
+public class Ex2Variable {
+    public static void main(String[] args) {
+        int varInt = -100;
+        double varDouble = 5.23;
+        String varString = "hello variable";
+        System.out.println(varInt);
+        System.out.println(varDouble);
+        System.out.println(varString);
+    }
+}
