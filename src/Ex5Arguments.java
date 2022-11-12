4a8691fd7b4dc259b30ccd3086e8a5eefee78b61
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-12 14:34:06 +0100

    exercice

---
+++ src/Ex5Arguments.java
@@ -0,0 +1,7 @@
+public class Ex5Arguments {
+    public static void main(String[] args) {
+        for (int i = 0; i < args.length; ++i) {
+            System.out.println(args[i]);
+        }
+    }
+}
