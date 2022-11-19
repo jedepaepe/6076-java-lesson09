a16f91a579f00c4ab1dfaac97e9b086856011d13
Jean-Michel Depaepe <jean-michel.depaepe@equans.com>
2022-11-19 14:16:44 +0100

    exercice

---
+++ src/ExOperators.java
@@ -0,0 +1,13 @@
+public class ExOperators {
+    public static void main(String[] args) {
+        System.out.println(10 + 5);
+        System.out.println(10 - 5);
+        System.out.println(10 * 5);
+        System.out.println(10 / 5);
+        System.out.println(10 % 5);
+        System.out.println(10 > 5);
+        System.out.println(10 < 5);
+        System.out.println(10 > 5 && 3 > 10);
+        System.out.println(10 > 5 || 3 > 10);
+    }
+}
